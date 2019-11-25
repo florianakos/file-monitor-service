df41a2e36b5cf79adf7b69b02cfb2709f65cec46
Florian Akos Szabo <florian.akos.szabo@gmail.com>
2019-11-25 14:31:35 +0100

    format shell code

--- monitor_service.sh
+++ monitor_service.sh
@@ -3,9 +3,8 @@
 TARGET=monitored_folder
 PROCESSED=processed_files
 
-inotifywait -m -e create -e moved_to --format "%f" $TARGET \
-| while read FILENAME
-    do
+inotifywait -m -e create -e moved_to --format "%f" $TARGET |
+	while read FILENAME; do
 		mv "$TARGET/$FILENAME" "$PROCESSED/$FILENAME"
 
 		ORIGSIZE=$(stat "$PROCESSED/$FILENAME" --print="%s")
@@ -16,9 +15,9 @@ inotifywait -m -e create -e moved_to --format "%f" $TARGET \
 
 		PERCENT=$(echo "scale=4 ; ($ORIGSIZE - $PROCSIZE) / $ORIGSIZE * 100" | bc)
 
-        LOG_MSG=$(echo "$(date "+%Y-%m-%d %H:%M:%S"): New file detected ($FILENAME). Original size: $ORIGSIZE --- Zipped size: $PROCSIZE --- Compression: $PERCENT%")
-        echo $LOG_MSG | tee -a service_history.log
-            
+		LOG_MSG=$(echo "$(date "+%Y-%m-%d %H:%M:%S"): New file detected ($FILENAME). Original size: $ORIGSIZE --- Zipped size: $PROCSIZE --- Compression: $PERCENT%")
+		echo "$LOG_MSG" | tee -a service_history.log
+
 		sqlite3 stats.db "INSERT INTO data (time, file, orig_size, comp_size, comp_rate) VALUES ($(date +%s), '${PROCESSED}/${FILENAME}.gz', $ORIGSIZE, $PROCSIZE, $PERCENT);"
 
-    done
+	done
